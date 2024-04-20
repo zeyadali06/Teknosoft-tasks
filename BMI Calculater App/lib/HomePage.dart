9305084690b465834344e7d591b1bda4d0076025
zeyad-1 <z01551153743@gmail.com>
2024-04-20 22:59:04 +0200

    create home page

--- BMI Calculater App/lib/HomePage.dart
+++ BMI Calculater App/lib/HomePage.dart
@@ -1,5 +1,10 @@
 // ignore_for_file: file_names
 
+import 'package:bmi_calculater_app/AgeField.dart';
+import 'package:bmi_calculater_app/GenderChoices.dart';
+import 'package:bmi_calculater_app/HeightBar.dart';
+import 'package:bmi_calculater_app/WeightBar.dart';
+import 'package:bmi_calculater_app/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,36 +12,42 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(30),
-        child: SizedBox(
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 30),
-              Text(
-                'BMI Calculator',
-                style: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.w900),
+        padding: const EdgeInsets.all(kPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Spacer(flex: 2),
+            const Text(
+              'BMI Calculator',
+              style: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.w900),
+            ),
+            const Spacer(flex: 2),
+            const Center(
+              child: Text(
+                'Gender',
+                style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.w900),
               ),
-              SizedBox(height: 10),
-              Center(
-                child: Text(
-                  'Gender',
-                  style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.w900),
-                ),
+            ),
+            const Spacer(flex: 1),
+            const GenderChoices(),
+            const Spacer(flex: 2),
+            const Center(
+              child: Text(
+                'Height (cm)',
+                style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.w900),
               ),
-              Center(
-                child: Text(
-                  'Height (cm)',
-                  style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.w900),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Column(
+            ),
+            const Spacer(flex: 2),
+            const HeightBar(),
+            const Spacer(flex: 2),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: Column(
                     children: [
                       Center(
                         child: Text(
@@ -44,9 +55,14 @@ class HomePage extends StatelessWidget {
                           style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.w900),
                         ),
                       ),
+                      SizedBox(height: 25),
+                      AgeField(),
                     ],
                   ),
-                  Column(
+                ),
+                SizedBox(width: 15),
+                Expanded(
+                  child: Column(
                     children: [
                       Center(
                         child: Text(
@@ -54,12 +70,26 @@ class HomePage extends StatelessWidget {
                           style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.w900),
                         ),
                       ),
+                      SizedBox(height: 25),
+                      WeightBar(),
                     ],
                   ),
-                ],
+                ),
+              ],
+            ),
+            const Spacer(flex: 2),
+            MaterialButton(
+              onPressed: () {},
+              minWidth: double.infinity,
+              height: 50,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(kRadius)),
+              color: const Color(0xFF2566cf),
+              child: const Text(
+                'CALCULATE BMI',
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.w800, fontSize: 16),
               ),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
