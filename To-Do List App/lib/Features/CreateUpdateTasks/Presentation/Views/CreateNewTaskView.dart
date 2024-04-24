4f3282eddfbbce9f130bef5729ebe0b63cff90f0
zeyad-1 <z01551153743@gmail.com>
2024-04-24 17:48:09 +0200

    create MVVM Pattern for tasks2

---
+++ To-Do List App/lib/Features/CreateUpdateTasks/Presentation/Views/CreateNewTaskView.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class CreateNewTaskView extends StatelessWidget {
+  const CreateNewTaskView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
